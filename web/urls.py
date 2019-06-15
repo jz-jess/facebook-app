c6907e654148f8540961ccdaafa5cf399ab601ce
Jesse <iamjess988@gmail.com>
2019-06-15 17:21:56 +0530

    ordering imports

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+
+from . import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
